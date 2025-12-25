8fa974beaf531a213a7b4fe9f3a2ce77c086aff2
Dzyker <grif.master80@gmail.com>
2025-12-25 11:19:23 +0300

    change app title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, NavLink, Navigate, Outlet, useLocation } from "react-router-dom"
+import { Routes, Route, NavLink, Navigate, Outlet } from "react-router-dom"
 import Home from './pages/Home'
 import Gallery from './pages/Gallery'
 import Favorites from './pages/Favorites'
